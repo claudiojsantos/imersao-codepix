011f8e120e2e97a7c0872a6688856e03ce765b45
Cláudio Santos <cljos@hotmail.com>
2023-10-18 08:35:55 -0300

    day 01

---
+++ domain/model/transaction.go
@@ -0,0 +1,98 @@
+package model
+
+import (
+	"errors"
+	"time"
+
+	"github.com/asaskevich/govalidator"
+	uuid "github.com/satori/go.uuid"
+)
+
+const (
+	TransactionPending   = "pending"
+	TransactionCompleted = "completed"
+	TransactionError     = "error"
+	TransactionConfirmed = "confirmed"
+)
+
+type TransactionRepositoryInterface interface {
+	Register(transaction *Transaction) error
+	Save(transaction *Transaction) error
+	Find(id string) (*Transaction, error)
+}
+
+type Transaction struct {
+	Base              `valid:"required"`
+	AccountFrom       *Account `json:"account_from" valid:"-"`
+	Amount            float64  `json:"amount" valid:"notnull"`
+	PixKeyTo          *PixKey  `json:"pix_key_to" valid:"-"`
+	Status            string   `json:"status" valid:"notnull"`
+	Description       string   `json:"description" valid:"notnull"`
+	CancelDescription string   `json:"cancel_description" valid:"-"`
+}
+
+type Transactions struct {
+	Transactions []*Transaction
+}
+
+func (transaction *Transaction) isValid() error {
+	_, err := govalidator.ValidateStruct(transaction)
+
+	if transaction.Amount <= 0 {
+		return errors.New("the amount must be greater than 0")
+	}
+
+	if transaction.Status != TransactionPending && transaction.Status != TransactionCompleted && transaction.Status != TransactionError {
+		return errors.New("the status must be pending, completed or error")
+	}
+
+	if transaction.PixKeyTo.AccountID == transaction.AccountFrom.ID {
+		return errors.New("the source and destination account cannot be the same")
+	}
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func NewTransaction(accountFrom *Account, amount float64, pixKeyTo *PixKey, description string, id string) (*Transaction, error) {
+	transaction := Transaction{
+		AccountFrom: accountFrom,
+		Amount:      amount,
+		PixKeyTo:    pixKeyTo,
+		Status:      TransactionPending,
+	}
+
+	transaction.ID = uuid.NewV4().String()
+	transaction.CreatedAt = time.Now()
+
+	err := transaction.isValid()
+	if err != nil {
+		return nil, err
+	}
+
+	return &transaction, nil
+}
+
+func (transaction *Transaction) Complete() error {
+	transaction.Status = TransactionCompleted
+	transaction.UpdatedAt = time.Now()
+	err := transaction.isValid()
+	return err
+}
+
+func (transaction *Transaction) Error() error {
+	transaction.Status = TransactionError
+	transaction.Description = "transaction rejected, insufficient funds"
+	transaction.UpdatedAt = time.Now()
+	err := transaction.isValid()
+	return err
+}
+
+func (transaction *Transaction) Confirm() error {
+	transaction.Status = TransactionConfirmed
+	transaction.UpdatedAt = time.Now()
+	err := transaction.isValid()
+	return err
+}
